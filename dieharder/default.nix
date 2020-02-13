2fbf345dce112fdcb87a00ba013fd696aeb2c5cc
Dominic Steinitz <dominic@steinitz.org>
2020-02-13 09:04:23 +0000

    Attempt to build dieharder

---
+++ dieharder/default.nix
@@ -0,0 +1,19 @@
+{stdenv, fetchurl, gsl}:
+
+stdenv.mkDerivation rec {
+  name = "dieharder-3.31.1";
+  src = fetchurl {
+    url = "http://webhome.phy.duke.edu/~rgb/General/dieharder/dieharder-3.31.1.tgz";
+    sha256 = "0zvj9g6y99fz41332q22g4kgnmf9ryf3acvlmi4kamac77w0zzvc";
+  };
+  nativeBuildInputs = [ gsl ];
+  buildInputs = [ ];
+  configureFlags = [ ];
+
+  meta = with stdenv.lib; {
+    description = "Suite of Random Number Tests";
+    license     = licenses.gpl2;
+    homepage    = http://webhome.phy.duke.edu/~rgb/General/dieharder.php;
+    maintainers = [ maintainers.idontgetoutmuch ];
+  };
+}
